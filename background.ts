7f964e4ef692cbe6dd180b96cbff5a9465cfcd9b
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2025-05-24 11:27:59 +0900

    ref background.ts

--- background.ts
+++ background.ts
@@ -1,24 +1,36 @@
 import type { Data } from './types'
 import { Storage } from '@plasmohq/storage'
-import { STORAGE_LABEL } from './constants'
+import { STORAGE_LABEL, DELAY } from './constants'
 
 const storage = new Storage()
 
-const _isUpdateDate = ({ updatedDate }: { updatedDate: Data['updatedDate'] }) => {
+const _isUpdateDateBefore = ({ updatedDate }: { updatedDate: Data['updatedDate'] }) => {
   const today = new Date()
   const updateDay = new Date(updatedDate)
 
   today.setHours(0, 0, 0, 0)
   updateDay.setHours(0, 0, 0, 0)
 
-  return updateDay <= today
+  return updateDay < today
 }
 
 const checkOpenTabs = async () => {
   const data: Data[] = await storage.get(STORAGE_LABEL)
 
   for (const v of data) {
+    v.elapsed = v.elapsed + DELAY
+
+    if (_isUpdateDateBefore({ updatedDate: v.updatedDate })) {
+      v.elapsed = 0
+      v.updatedDate = new Date().getTime()
+    }
+
+    if (v.elapsed >= v.duration) {
+      console.log('over duration')
+    }
   }
+
+  await storage.set(STORAGE_LABEL, data)
 }
 
-setInterval(() => checkOpenTabs(), 5000)
+setInterval(() => checkOpenTabs(), DELAY)
--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
 export const STORAGE_LABEL = 'CLOCKLOCK_DATA'
+export const DELAY = 5000
