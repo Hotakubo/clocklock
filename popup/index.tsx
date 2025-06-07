7e44351c01f853fdcc9922df182b9ea3e3c75689
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2025-06-07 22:16:16 +0900

    popup

--- popup/index.tsx
+++ popup/index.tsx
@@ -2,10 +2,22 @@ import type { Data } from '~/shared/types'
 import React, { useEffect, useState } from 'react'
 import { Storage } from '@plasmohq/storage'
 import { STORAGE_LABEL, DELAY } from '~/shared/constants'
-import { format } from 'date-fns'
+import { intervalToDuration } from 'date-fns'
 import '~/shared/style.css'
 
 const storage = new Storage()
+
+const _parseElapsed = ({ elapsed }: { elapsed: number }): string => {
+  const duration = intervalToDuration({
+    start: 0,
+    end: elapsed
+  })
+  const hours = duration.hours ? `${duration.hours}` : 0
+  const minutes = duration.minutes ? `${duration.minutes}` : 0
+  const seconds = duration.seconds ? `${duration.seconds}` : 0
+
+  return `${hours}:${minutes}:${seconds}`
+}
 
 const Popup = () => {
   const [data, dataSet] = useState<Data[]>([])
@@ -30,7 +42,7 @@ const Popup = () => {
         {data.map((v, index) => (
           <li key={index} className="flex gap-1">
             <div>{v.domain}</div>
-            <div>{format(v.elapsed * 1000, 'HH:mm:ss')}</div>
+            <div>{_parseElapsed({ elapsed: v.elapsed })}</div>
           </li>
         ))}
       </ul>
