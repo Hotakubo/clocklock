259c60d91201fb14baef9dad222153220fe68491
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2025-06-07 23:30:58 +0900

    diff

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,8 +1,8 @@
 import type { Data } from '~/shared/types'
 import React, { useEffect, useState } from 'react'
+import { intervalToDuration, differenceInMilliseconds } from 'date-fns'
 import { Storage } from '@plasmohq/storage'
 import { STORAGE_LABEL, DELAY } from '~/shared/constants'
-import { intervalToDuration } from 'date-fns'
 import '~/shared/style.css'
 
 const storage = new Storage()
@@ -14,15 +14,29 @@ const _parseElapsed = ({
   startDuration: Data['duration'];
   elapsed: Data['elapsed'];
 }): string => {
+  const diff = differenceInMilliseconds(new Date(elapsed), new Date(startDuration))
+
   const duration = intervalToDuration({
-    start: startDuration,
-    end: startDuration + elapsed
+    start: 0,
+    end: Math.abs(diff)
   })
   const hours = duration.hours ? String(duration.hours).padStart(2, '0') : '00'
   const minutes = duration.minutes ? String(duration.minutes).padStart(2, '0') : '00'
   const seconds = duration.seconds ? String(duration.seconds).padStart(2, '0') : '00'
 
-  return `${hours}:${minutes}:${seconds}`
+  return `${diff > 0 ? '-' : ''}${hours}:${minutes}:${seconds}`
+}
+
+const _durationStyle = ({
+  elapsed,
+  duration
+}: {
+  elapsed: Data['elapsed'];
+  duration: Data['duration'];
+}) => {
+  const diff = differenceInMilliseconds(new Date(elapsed), new Date(duration))
+
+  return diff > 0 ? 'text-red-600' : ''
 }
 
 const Popup = () => {
@@ -50,9 +64,9 @@ const Popup = () => {
     <div className="p-4">
       <ul className="list-none">
         {data.map((v, index) => (
-          <li key={index} className="flex gap-1 text-lg">
+          <li key={index} className="flex gap-2 text-lg">
             <div>{v.domain}</div>
-            <div>{_parseElapsed({
+            <div className={`${_durationStyle({ elapsed: v.elapsed, duration: v.duration })}`}>{_parseElapsed({
               startDuration: v.duration,
               elapsed: v.elapsed
             })}</div>
