4239a62cc5de962ea8fec62aaf4c08c44d99a29b
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2025-06-08 18:04:37 +0900

    contents

--- contents/index.tsx
+++ contents/index.tsx
@@ -115,7 +115,7 @@ const Cover = () => {
         height: `${height}px`
       }}
     >
-      <div className="grid w-full h-full justify-center items-center backdrop-blur-md bacground-gray-200 bg-opacity-90"></div>
+      <div className="grid w-full h-full justify-center items-center backdrop-blur-md"></div>
     </div>
   )
 }
